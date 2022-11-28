461814e63604b8f9a0797b4b24311cf8d0d9e797
ThangCN18 <thang49082@donga.edu.vn>
2022-11-29 00:07:44 +0700

    m

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use('/users', userRoutes)
 require('./connection')
 
 const server = require('http').createServer(app);
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 const io = require('socket.io')(server, {
   cors: {
     origin: 'http://localhost:3000',
